8302b631faed8691339aeb19ed7058b68a9679fe
Roycem5x <roycem5x@gmail.com>
2021-03-26 11:33:43 -0400

    Fixed the trinomial factor algorithm. There were some miscalculations because the for loop in python looping from 0, n-1

--- Commands/Math.py
+++ Commands/Math.py
@@ -9,31 +9,45 @@ class Math(commands.Cog):
         self.client = client
 
     @commands.command(name = "getFactor", description = "Give three numbers seperated by spaces to get the factor of a trinomial")
-    async def getFactor(self, ctx, *args):
-        if len(args) != 3:
-            return ctx.send("The length of the trinomial is invalid"+command_format)      
-
+    async def getFactor(self, ctx, arg0, arg1, arg2):
         try:
+            # Convert args to int
+            arg0, arg1, arg2 = int(arg0), int(arg1), int(arg2)
 
-            args = [int(i) for i in args] 
+            a = arg0
+            b = arg1
+            c = arg2
+
+            # The numbers that will be added and multiplied
+            add_num = b
+            mult_num = a*c
+
+            # Check the smallest num
+            # mult_num is also added since we needed to check any numbers between the mult num too
+            smallest_num = min(arg0, arg1, arg2, mult_num)
+            biggest_num = max(arg0, arg1, arg2, mult_num)
+
+            """
+            Loop from the smallest number to the biggest number, 
+                providing all possible combinations of numbers
             
-            add_num = args[1]
-            mult_num = args[0] * args[2]
-
-            for i in range((args[0] if args[0] < 0 else 0), mult_num):
-                for x in range(mult_num , i, -1):
+            It is biggest num + 1 because in python,
+                the loop goes from smallest_num to biggest_num - 1
+            
+            """
+            for i in range(smallest_num, biggest_num + 1):
+                for x in range(biggest_num, i + 1, -1):
                     if i+x == add_num and i*x == mult_num:
-                        if args[0] > 1:
-                            await ctx.send("{}x^2 + {}x + {}x + {}".format(args[0], i, x, args[2]))
+                        if a != 1:
+                            await ctx.send("`{}x^2` + `{}x` + `{}x` + `{}`".format(a, i, x, c))
                         else:
-                            await ctx.send("(x + {})(x + {})".format(i, x))
-                        
+                            await ctx.send("`(x + {})(x + {})`".format(i, x))
                         return;
 
             await ctx.send("The passed values cannot be factored."+command_format)
 
 
-        except ValueError:
+        except Exception:
             await ctx.send(command_format)
         
 
