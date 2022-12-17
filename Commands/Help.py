270f08c7d97dc24e2ff077b75c7991ec1b9ed142
royce-mathew <royce1mathew@gmail.com>
2022-12-17 16:12:01 -0500

    Added Guild specific prefixes

--- Commands/Help.py
+++ Commands/Help.py
@@ -1,24 +1,23 @@
 import discord
 from discord.ext import commands
-from Data.functions import create_embed
+from modules.functions import create_embed
 
-from modules.json_handler import GuildData
-
-guild_data = GuildData()
+from modules.data_handler import GuildData
 
 
 class Help(commands.Cog):
 
     # The first thing that happens when this class is called
-    def __init__(self, client):
+    def __init__(self, client: discord.Client):
         self.client = client
 
     @commands.command(name="Help", description="Returns all the available commmands")
-    async def help(self, ctx):
-        prefix = guild_data.get_guild_data(ctx.message.guild)["prefix"]
+    async def help(self, ctx: commands.Context):
+        local_guild_data = GuildData.get_guild_data(ctx.guild)
+        prefix = local_guild_data["prefix"]
         embed = create_embed(
             "Help Command",
-            f"These are the avaliable commands for **{ctx.message.guild.name}**\nThe client prefix is: `{prefix}`"
+            f"These are the avaliable commands for **{ctx.guild.name}**\nThe client prefix is: `{prefix}`"
         )
 
         for command in self.client.commands:
@@ -27,5 +26,5 @@ class Help(commands.Cog):
         await ctx.send(embed=embed)
 
 
-async def setup(client):
+async def setup(client: discord.Client):
     await client.add_cog(Help(client))
