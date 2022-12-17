7e1197cbcacbcb44d9ef8d239477bc2360ee3f8f
royce-mathew <royce1mathew@gmail.com>
2022-12-17 16:26:14 -0500

    Removed Dorepeat from ErrorHandler

--- Commands/ErrorHandler.py
+++ Commands/ErrorHandler.py
@@ -63,30 +63,6 @@ class ErrorHandler(commands.Cog):
             print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
-    """Below is an example of a Local Error Handler for our command do_repeat"""
-
-    @commands.command(name='repeat', aliases=['mimic', 'copy'])
-    async def do_repeat(self, ctx: commands.Context, *, inp: str):
-        """A simple command which repeats your input!
-        Parameters
-        ------------
-        inp: str
-            The input you wish to repeat.
-        """
-        await ctx.send(inp)
-
-    @do_repeat.error
-    async def do_repeat_handler(self, ctx: commands.Context, error):
-        """A local Error Handler for our command do_repeat.
-        This will only listen for errors in do_repeat.
-        The global on_command_error will still be invoked after.
-        """
-
-        # Check if our required argument inp is missing.
-        if isinstance(error, commands.MissingRequiredArgument):
-            if error.param.name == 'inp':
-                await ctx.send("You forgot to give me input to repeat!")
-
 
 async def setup(client: discord.Client):
     await client.add_cog(ErrorHandler(client))
